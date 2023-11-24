1431952660d4046bb846937369a971a6cab3cd8e
francois.vandeputte <francois.vandeputte@student.vinci.be>
2023-11-25 00:52:04 +0100

    feat: Added startround in game loop

--- commands/game.js
+++ commands/game.js
@@ -1,12 +1,23 @@
 const { SlashCommandBuilder } = require('discord.js');
 const init = require("../lib/init");
+const gameStartRound = require("../lib/gameStartRound");
+const Roles = require("../lib/Roles");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('game')
         .setDescription('Starts a werewolf game'),
     async execute(interaction) {
+        let lovers = [];
+
         const { players, channels } = await init(interaction);
+
+        await gameStartRound(
+            players,
+            channels.find(c => c.role === Roles.THIEF)?.channel,
+            channels.find(c => c.role === Roles.CUPID)?.channel,
+            lovers
+        );
 
         //TODO Preparation tour
 
